88a1f52237611280d9fc12fece897dd73d57bd5b
odntht <odntht@gmail.com>
2019-01-16 21:03:29 -0200

    Update Algoritmos de Ordenacao.py
    Adicionando o Shell Sort no algoritmo.

--- Algoritmos de Ordenacao.py
+++ Algoritmos de Ordenacao.py
@@ -1,15 +1,18 @@
 import random
 import time
 
-vetor = list(range(0,10000)) #gera vetor
-random.shuffle(vetor)
+vetor = list(range(0,1000)) #gera vetor com mil posições, se quiser mais ou menos só mudar o segundo valor
+random.shuffle(vetor) #funcao que embaralha os valores do vetor
+
+print(vetor) #apenas exibe o vetor já embaralhado (em vetores de até uns 50 valores é ok visualizar, mas se expandir pra vetores de mil valores ou mais, já nao fica bom, ai recomendo comentar essa linha...)
 
 ##Cada vX desse corresponde a um algoritmo de ordenação, dessa forma é posssivel testar a performace com o mesmo vetor em todos eles!
 vb = vetor #Bubble Sort
 vs = vetor #Selection Sort
 vi = vetor #Insertion Sort
+vh = vetor #Shell Sort
 
-def bubble_sort(vb):
+def bubble_sort(vb): #funcao Bubble Sort
 
     fim = len(vb)
 
@@ -25,15 +28,13 @@ def bubble_sort(vb):
             i += 1
         fim -= 1
 
-
-
 antes = time.time()
 bubble_sort(vb)
 depois = time.time()
 
 total = (depois - antes)*1000
 
-print("Bubble sort: %0.2f ms" % total)
+print("Bubble sort: %0.2f ms" % total) #Exibe o tempo total de execução deste algoritmo
 
 def selection_sort(vs):
     i = 0
@@ -58,7 +59,7 @@ depois = time.time()
 
 total = (depois - antes)*1000
 
-print("Selection Sort: %0.2f ms" % total)
+print("Selection Sort: %0.2f ms" % total) #Exibe o tempo total de execução deste algoritmo
 
 def insertion_sort(vi):
     i = 1
@@ -81,5 +82,34 @@ depois = time.time()
 
 total = (depois - antes)*1000
 
+print("Insertion Sort: %0.2f ms" % total) #Exibe o tempo total de execução deste algoritmo
 
-print("Insertion Sort: %0.2f ms" % total)
+def shell_sort(vh):
+
+    h = len(vh)//2
+
+    while h > 0:
+        i = h
+
+        while i < len(vh):
+            temp = vh[i]
+            trocou = False
+            j = i -h
+
+            while j >= 0 and vh[j] > temp:
+                vh[j+h] = vh[j]
+                trocou = True
+                j -= h
+
+            if trocou:
+                v[j+h] = temp
+            i+= 1
+        h = h // 2
+
+antes = time.time()
+shell_sort(vh)
+depois = time.time()
+
+total = (depois - antes)*1000
+
+print("Shell Sort: %0.2f ms" % total) #Exibe o tempo total de execução deste algoritmo
